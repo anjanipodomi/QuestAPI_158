f542b682960c095292b2a64e57c5f7eb4fb5925a
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 20:20:55 +0700

    Menambahkan composable FormTambahSiswa

--- app/src/main/java/com/example/localrestapi/view/HalamanEntry.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanEntry.kt
@@ -65,3 +65,11 @@ fun EntrySiswaBody(
         }
     }
 }
+
+@Composable
+fun FormTambahSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+)
