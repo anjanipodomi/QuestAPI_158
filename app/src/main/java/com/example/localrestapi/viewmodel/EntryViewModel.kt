738f74583a26476c059a07aeee57702f614a92a7
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:34:49 +0700

    membuat view model untuk mengelola input data siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -1,4 +1,7 @@
 package com.example.localrestapi.viewmodel
 
-class EntryViewModel {
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 }
