2075a9e860f09afe00bd1bda1b67b07d0854c891
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 21:10:48 +0700

    Perbaikan pengaturan navigasi dengan menambahkan satu aturan dasar untuk destinasi layar, membenahi penulisan tujuan halaman agar saling terhubung dengan benar, serta memperbaiki pemanggilan resource aplikasi sehingga setiap halaman (home dan entry) dapat dikenali dan digunakan dengan baik dalam alur navigasi aplikasi.

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiNavigasi.kt
@@ -1,8 +1,6 @@
 package com.example.localrestapi.uicontroller.route
 
-import com.example.mydatasisw.R
-
 interface DestinasiNavigasi {
-    override val route = "item_entry"
-    override val titleRes = R.string.entry_siswa
+    val route : String
+    val titleRes: Int
 }
