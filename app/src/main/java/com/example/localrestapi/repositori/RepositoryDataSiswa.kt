84d2de815971dd829055ca81cc13e1415073b94d
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:01:44 +0700

    membuat repository untuk mengambil dan menyimpan data siswa

--- app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,15 @@
 package com.example.localrestapi.repositori
 
+import com.example.localrestapi.apiservice.ServiceApiSiswa
+
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiwa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
