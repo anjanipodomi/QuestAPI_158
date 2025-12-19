25a188046bcc5a4776a502981eb5c4352ea32662
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 20:28:58 +0700

    Menambahkan AppBar dan tombol tambah pada HomeScreen

--- app/src/main/java/com/example/localrestapi/view/HalamanHome.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanHome.kt
@@ -8,5 +8,28 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        }
+    )
 }
