a32296d4fa1f50701f1271c86dcf3f63da290620
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:40:25 +0700

    menambahkan import

--- app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.localrestapi.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.mydatasisw.repositori.AplikasiDataSiswa
+import com.example.mydatasisw.viewmodel.EntryViewModel
+import com.example.mydatasisw.viewmodel.HomeViewModel
+
 fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
                 AplikasiDataSiswa
