72750d4bc7bfd19e0cffb779a51a7884c03608a1
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 20:27:34 +0700

    Membuat composable HomeScreen dengan ViewModel

--- app/src/main/java/com/example/localrestapi/view/HalamanHome.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanHome.kt
@@ -1,2 +1,12 @@
 package com.example.localrestapi.view
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    navigateToItemUpdate: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
