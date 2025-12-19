7c2e7e75882ae9fc18a1727b86fb64f33093d2ca
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:39:48 +0700

    menambahkan penyedia view model untuk halaman home dan input data siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
@@ -4,4 +4,11 @@ fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
                 AplikasiDataSiswa
         )
-
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container
+            .repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container
+            .repositoryDataSiswa) }
+    }
+}
