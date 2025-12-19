347eea63a011973dbf73464767517e4cad7e1aa5
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:18:28 +0700

    menambahkan import di DestinasiNavigasi.kt dan DestinasiHome.kt

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiHome.kt
@@ -1,5 +1,7 @@
 package com.example.localrestapi.uicontroller.route
 
+import com.example.mydatasisw.R
+
 object DestinasiHome {
     override val route = "home"
     override val titleRes = R.string.app_name
--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiNavigasi.kt
@@ -1,5 +1,7 @@
 package com.example.localrestapi.uicontroller.route
 
+import com.example.mydatasisw.R
+
 interface DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
