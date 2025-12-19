17eaf7544ce9772ff8e3e4b91cf3adc7a53c30a2
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:27:03 +0700

    menyiapkan komponen header aplikasi untuk halaman data siswa

--- app/src/main/java/com/example/localrestapi/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/localrestapi/view/SiswaTopAppBar.kt
@@ -1,2 +1,13 @@
 package com.example.localrestapi.view
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+
+}
