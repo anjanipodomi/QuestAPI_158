3d263a0c3f251c792465ae02081238a893a5d09b
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:02:51 +0700

    membuat container aplikasi untuk menyiapkan koneksi dan pengelolaan data siswa

--- app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
@@ -1,2 +1,39 @@
 package com.example.localrestapi.repositori
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+interface ContainerApp{
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply{
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(interceptor = logging)
+        .build()
+
+    private val retrofit: Retrofit.Builder()
+    .baseUrl(baseUrl= baseurl)
+    .addConverterFactory(
+    factory= Json{
+        ignoreUnknownKeys = true
+        prettyPrint = true
+        isLenient = true
+    }.asConverterFactory(contentType="application/json".toMediaType())
+    )
+    .client(client= klien)
+    .build()
+
+    private val retrofitService : ServiceApiSiswa by lazy{
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(serviceApiSiswa= retrofitService) }
+}
+
