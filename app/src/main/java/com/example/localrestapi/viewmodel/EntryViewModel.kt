42af4115431c6722bd475bbff8af89439d212292
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:36:02 +0700

    menambahkan pengecekan agar data siswa tidak boleh kosong saat diinput

--- app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -4,4 +4,12 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
     ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
 }
