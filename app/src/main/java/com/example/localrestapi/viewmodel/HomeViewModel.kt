40fb91872d2f2b8f7b120d8ef65433bfc99df6b1
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-19 16:29:32 +0700

    membuat penanda status tampilan untuk data siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/HomeViewModel.kt
@@ -1,2 +1,7 @@
 package com.example.localrestapi.viewmodel
 
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
