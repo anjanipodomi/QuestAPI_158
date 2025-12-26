f753641e1acf526fa20b62c0a96feb42eb5ebf36
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-26 14:47:09 +0700

    menambahkan konfigurasi navigasi untuk halaman detail siswa

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -1,4 +1,9 @@
 package com.example.localrestapi.uicontroller.route
 
 object DestinasiDetail {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
