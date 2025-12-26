40a43b55e3ba4d83a9a3e79e23e0dc8afcc9c2c9
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-26 14:47:44 +0700

    menambahkan konfigurasi navigasi untuk halaman edit siswa

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEdit.kt
@@ -1,4 +1,9 @@
 package com.example.localrestapi.uicontroller.route
 
 object DestinasiEdit {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
