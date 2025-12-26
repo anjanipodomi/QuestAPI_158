bc511dd8799b9aa17ced92a52c7285beef24f1a8
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-26 13:44:40 +0700

    menambahkan status UI untuk menampilkan kondisi sukses, loading, dan error pada detail siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.localrestapi.viewmodel
 
+import
+
+sealed interface StatusUIDetail {
+    data class Success(val siswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
