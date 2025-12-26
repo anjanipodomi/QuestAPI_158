fd88d632a59f95351e26bfa44404b399b3d4fc36
anjanipodomi <anjanidihapsari@gmail.com>
2025-12-26 14:37:34 +0700

    menambahkan state UI dan pengambilan id siswa pada ViewModel edit

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -3,4 +3,13 @@ package com.example.localrestapi.viewmodel
 class EditViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryDataSiswa: RepositoryDataSiswa
-) : ViewModel()
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+
+}
